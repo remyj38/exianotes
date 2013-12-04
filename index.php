3150e05714ef6c68a374ad31c95218360734fe56
remyj38 <remmonster@hotmail.fr>
2013-12-04 16:01:38 +0100

    Modification Mineure

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once 'config.php';
 require_once 'fonctions.php';
 init_classes();
 
-//essais de connexion à la base de donnée
+//essais de connexion à la base de donnée.
 test_sql();
 $auth = new auth();
 if (!$auth) {
